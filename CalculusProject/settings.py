1fd631da4a562197aa7a981249d11527417679e1
mohammadaminmg10 <ghorbannezhadm2@gmail.com>
2023-05-16 11:35:52 +0330

    fixing settings

--- CalculusProject/settings.py
+++ CalculusProject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'CalculusProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': ['templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
