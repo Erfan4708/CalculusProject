b2f4ba0faeddace970e4f83804f5d2515b1e38e4
Erfan Abbasi <abbasy.4708@gmail.com>
2023-05-16 20:24:43 +0330

    edit base and post list in templates

--- CalculusProject/settings.py
+++ CalculusProject/settings.py
@@ -122,4 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
