634f81734769d7872c68435641c911ec82370292
mohammadaminmg10 <ghorbannezhadm2@gmail.com>
2023-05-18 07:07:37 +0330

    fixing fav-view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from .models import Post
 from django.views.generic import ListView, DetailView
 from django.db import models
-from django.shortcuts import get_object_or_404 ,render
-from .forms import PostForm , FavoriteForm
-from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
+from django.shortcuts import get_object_or_404, render
+from .forms import PostForm, FavoriteForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class PostList(ListView):
@@ -13,15 +13,16 @@ class PostList(ListView):
     context_object_name = "posts"
     paginate_by = 6
 
-class FavoriteList(LoginRequiredMixin,ListView):
+
+class FavoriteList(LoginRequiredMixin, ListView):
     model = Post
-    template_name = '../templates/post_favorite.html'
-    image = models.ImageField(upload_to='images/')
-    context_object_name = "post_f"
+    template_name = 'post_favorite.html'
+    context_object_name = 'post_f'
     paginate_by = 6
-    # def test_func(self):
-    #     obj = self.get_object()
-    #     return obj.user == self.request.user
+
+    def get_queryset(self):
+        return self.model.objects.filter(favorite=True, author=self.request.user)
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     form = FavoriteForm(initial={'favorite': post.favorite})
@@ -31,4 +32,3 @@ def post_detail(request, pk):
             post.favorite = form.cleaned_data['favorite']
             post.save()
     return render(request, '../templates/post_detail.html', {'post': post, 'form': form})
-
